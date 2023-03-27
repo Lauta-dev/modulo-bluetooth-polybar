d188507d3ebdcfe51738d8e7ffa2a6b08ed55541
Lautaro <altairdiaz245@gmail.com>
2023-03-27 12:44:54 -0300

    Primer commit

---
+++ bluetooth.sh
@@ -0,0 +1,22 @@
+bluetooth() {
+  local bt=$(bluetoothctl show | grep "Powered" | cut -d ":" -f2)
+  local info=$(bluetoothctl info)
+  local dispositivo=$(bluetoothctl info | grep "Alias" | cut -d ":" -f2)
+
+  if [ $bt = "yes" ] 
+  then
+    if [[ $info = "Missing device address argument" ]]
+    then
+      # bluetooth pero sin conectar dispositivo
+      echo "%{F#abb2bf}"
+    else
+      # bluetooth con dispositivo conectado
+      echo "" $dispositivo
+    fi
+  else
+    # bluetooth apagado
+    echo "%{F#5c6370}"
+  fi
+}
+
+bluetooth
