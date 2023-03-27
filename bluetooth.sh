6b76fde3ef9ed1e8564c2ff2b5f1f98b56f1197a
Lautaro <altairdiaz245@gmail.com>
2023-03-27 12:51:15 -0300

    Primer commit

--- bluetooth.sh
+++ bluetooth.sh
@@ -7,14 +7,14 @@ bluetooth() {
   then
     if [[ $info = "Missing device address argument" ]]
     then
-      # bluetooth pero sin conectar dispositivo
+      # bluetooth activado pero sin conectar dispositivo
       echo "%{F#abb2bf}"
     else
       # bluetooth con dispositivo conectado
       echo "" $dispositivo
     fi
   else
-    # bluetooth apagado
+    # bluetooth desactivado
     echo "%{F#5c6370}"
   fi
 }
